4392395638b2bd5f92707974db1f83e8868147a7
Vijay Tiwari <vijay.tiwari.vj7>
2020-09-29 16:44:40 +0530

    Updates

---
+++ precache-manifest.249ad0e0928274dbda8e1f9c0fe00598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e43d32922ee6e11a65c70adc7289e4f",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "cee2244eaf047df4405e",
+    "url": "/clock/static/css/main.a857128f.chunk.css"
+  },
+  {
+    "revision": "bfb443bd453df94a4bb6",
+    "url": "/clock/static/js/2.930cb308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.930cb308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee2244eaf047df4405e",
+    "url": "/clock/static/js/main.b3a72140.chunk.js"
+  },
+  {
+    "revision": "9fbb0c64d7d7588fb297",
+    "url": "/clock/static/js/runtime-main.bf40dc10.js"
+  }
+]);
